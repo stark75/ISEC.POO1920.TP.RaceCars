dc1885e0979428797c4cb7ae280ff0f5c99f9a32
Diogo <dlsequeira75@gmail.com>
2019-11-10 00:06:22 +0000

    Changed: Car Class
    Changed energy and max energy variable type from int to double; Added movement and emergencySignal variables and their getters; Added energy manipulation

--- TP-POO1920-RaceCars/Car.cpp
+++ TP-POO1920-RaceCars/Car.cpp
@@ -4,7 +4,11 @@
 char Car::nextID = 'a';
 
 Car::Car(int mE, int mS, int e, std::string b, std::string m)
-	:brand(b), model(m), maxEnergy(mE), maxSpeed(mS), speed(0), time(0)
+	:brand(b), model(m), id(nextID),
+	maxEnergy(mE), maxSpeed(mS),
+	driver(nullptr), speed(0),
+	time(0), movement(false),
+	emergencySignal(false)
 {
 	if (e > maxEnergy)
 		energy = maxEnergy;
@@ -16,9 +20,6 @@ Car::Car(int mE, int mS, int e, std::string b, std::string m)
 			energy = e;
 	}
 
-	driver = nullptr;
-	id = nextID;
-	
 	if (nextID == 'z' || nextID == '?')
 	{
 		nextID = '?';
@@ -53,7 +54,7 @@ char Car::getID() const
 	return id;
 }
 
-int Car::getMaxEnergy() const
+double Car::getMaxEnergy() const
 {
 	return maxEnergy;
 }
@@ -68,7 +69,7 @@ Pilot* Car::getDriver() const
 	return driver;
 }
 
-int Car::getEnergy() const
+double Car::getEnergy() const
 {
 	return energy;
 }
@@ -83,6 +84,16 @@ int Car::getTime() const
 	return time;
 }
 
+bool Car::getMovement() const
+{
+	return movement;
+}
+
+bool Car::getEmergencySignal() const
+{
+	return emergencySignal;
+}
+
 
 std::string Car::getAsString() const
 {
@@ -95,11 +106,36 @@ std::string Car::getAsString() const
 	tmpString += " ";
 	tmpString += getModel();
 	tmpString += "]";
-
 	
 	return tmpString;
 }
 
+bool Car::accelerate(int times)
+{
+	return false;
+}
+
+bool Car::brake(int times)
+{
+	return false;
+}
+
+bool Car::energyCharge(int n)
+{
+	return false;
+}
+
+bool Car::energyConsumption()
+{
+	if (energy <= 0)
+	{
+		energy = 0;
+		return false;
+	}
+	energy -= speed * 0.1;
+	if (energy < 0) energy = 0;
+	return true;
+}
 
 std::ostream& operator<<(std::ostream& os, const Car& c)
 {
--- TP-POO1920-RaceCars/Car.h
+++ TP-POO1920-RaceCars/Car.h
@@ -11,32 +11,42 @@ class Car
 	char id;
 
 	//Car Specs
-	const int maxEnergy;
+	const double maxEnergy;
 	const int maxSpeed;
 
 	//Car Stats
 	Pilot* driver;
-	int energy;
+	double energy;
 	int speed;
 	int time;
-	
+	bool movement;
+	bool emergencySignal;
 		
 public:
 	Car(int mE, int mS, int e, std::string b, std::string m = "Base");
 	~Car();
 
+	//Getters
 	char getNextID() const;
 	std::string getBrand() const;
 	std::string getModel() const;
 	char getID() const;
-	int getMaxEnergy() const;
+	double getMaxEnergy() const;
 	int getMaxSpeed() const;
 	Pilot* getDriver() const;
-	int getEnergy() const;
+	double getEnergy() const;
 	int getSpeed() const;
 	int getTime() const;
-	
+	bool getMovement() const;
+	bool getEmergencySignal() const;
 	std::string getAsString() const;
+
+	//TODO: Car's actions
+	bool accelerate(int times = 1);
+	bool brake(int times = 1);
+	bool energyCharge(int n);
+	bool energyConsumption();
+
 };
 
 std::ostream& operator<<(std::ostream& os, const Car& c);
