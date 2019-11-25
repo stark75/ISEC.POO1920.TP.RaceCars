0b50012acb4d57de1a3c68a89a1a4ee192a509e0
Diogo <dlsequeira75@gmail.com>
2019-11-25 04:48:33 +0000

    Changed: Pilot Class
    Added some attributes

--- TP-POO1920-RaceCars/Pilot.h
+++ TP-POO1920-RaceCars/Pilot.h
@@ -1,5 +1,21 @@
 #pragma once
+#include "Car.h"
+
 class Pilot
 {
+    //Pilot ID
+    const char name; //TODO: Name is unique per pilot
+    
+
+    //Pilot Status
+    //Car* current;
+    //TODO: RACE
+
+
+
+public:
+	Pilot();
+	~Pilot();
+
 };
 
