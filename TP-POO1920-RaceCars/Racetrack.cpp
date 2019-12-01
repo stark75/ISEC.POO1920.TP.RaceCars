2f60896f767e382e884dacb0d11b37cebefc0135
Diogo <dlsequeira75@gmail.com>
2019-12-01 00:40:15 +0000

    Changed: VS Files and Racetrack Class
    Racetrack Class is not working yet

--- TP-POO1920-RaceCars/Racetrack.cpp
+++ TP-POO1920-RaceCars/Racetrack.cpp
@@ -1,112 +1,126 @@
 #include <iostream>
-#include "Racetrack.h"
 #include <vector>
+#include "Racetrack.h"
 
-Racetrack::Racetrack(std::string r, int cIT, int cIG, int nMC, int rTL)
-	raceTrack(r),
-	carsInTrack (cIT),
-	carsInGarage (CIG),
-	isMaxCars(nMC),
-	rTLength(rTL),
-	isCarMax(false);
+std::vector<std::string> Racetrack::raceTrackNames;
 
-
-	std::string Racetrack::getraceTrack() const
+Racetrack::Racetrack(int newMaxCars, int newLength, std::string newName): maxCars(newMaxCars), trackLength(newLength)
+{
+	if (raceTrackNames.empty())
 	{
-		return raceTrack;
+		name = newName;
+		raceTrackNames.push_back(name);
 	}
-
-
-	int Racetrack::getnMaxCars() const
+	else
 	{
-		int maxCars = 0;
-		int sizeInRT = getnCars;
-		if (maxCars <= sizeInRT) {
-
-			cout << "O número de carros não ultrapassa o limite (" << sizeInRT << "/" << maxCars << ")"
-		}
-
-		return nMaxCars;
-	}
-
-
-	int Racetrack::getrTLength() {
-		return rTLength;
-	}
-
-
-	int Racetrack::getncars()
-	{
-		int sizeGarage = static_cast<Car*>(carsInGarage.size());
-		int sizeInRace = static_cast<Car*>(carsInTrack.size());
-		int sizeInRT = sizeGarage + sizeInRace;
-
-		cout << "O número de carros na Racetrack é " << sizeInRT;
-
-		return sizeInRT;
-	}
-
-
-	bool Racetrack::getisCarMax() 
-	{
-		if (nCars == nMaxCars)
+		for (int i = 0; i < static_cast<int>(raceTrackNames.size()); i++)
 		{
-			return true;
-		}
-
-		else
-		{
-			return false;
-		}
-	}
-
-	int RaceTrack::getcarsInTrack()
-	{
-		return carsInTrack;
-	}
-
-
-	int RaceTrack::getcarsInGarage()
-	{
-		return carsInGarage;
-	}
-
-
-	int Racetrack::trackToGarage()
-	{
-		int times = 0;
-		auto it = std::next(carsInTrack<Car*>(), getnCars()), times++;
-		std::move(carsInTrack<Car*>.begin(), it, std::back_inserter(carsInGarage<Car*>));
-		carsInTrack<Car*>.erase(carsInTrack.begin(), it);
-		cout << "Foram " << times << "carros para a garagem";
-		return times;
-	}
-
-
-	int Racetrack::garageToTrack()
-	{
-		int times = 0;
-		auto it = std::next(carsInGarage(), getnCars()), times++;
-		std::move(carsInGarage.begin(), it, std::back_inserter(carsInTrack<Car*>));
-		carsInGarage.erase(carsInGarage.begin(), it);
-		cout << "Foram " << times << "carros para a track";
-		return times;
-	}
-
-	
-	int Racetrack::moveRaceTrack() {
-
-		std::vector<int> Racetrack::getinRace()
-		{
-			return std::vector<string> inRace;
-			/*if(returned == car called){
-				cout << "Não pode mover o carro porque ele está neste momento a correr";
+			if (raceTrackNames[i] == newName)
+			{
+				newName += 1;
 			}
-
-			else{
-				//Mover o carro
-			}
-			*/
 		}
 
+		name = newName;
+		raceTrackNames.push_back(name);
 	}
+}
+
+std::string Racetrack::getRaceTrack() const
+{
+	return raceTrack;
+}
+
+
+int Racetrack::getnMaxCars() const
+{
+	int maxCars = 0;
+	int sizeInRT = getnCars;
+	if (maxCars <= sizeInRT) {
+		std::cout << "O número de carros não ultrapassa o limite (" << sizeInRT << "/" << maxCars << ")"
+	}
+
+	return maxCars;
+}
+
+
+int Racetrack::getTrackLength() {
+	return trackLength;
+}
+
+
+int Racetrack::getncars()
+{
+	int sizeGarage = static_cast<Car*>(carsInGarage.size());
+	int sizeInRace = static_cast<Car*>(carsInTrack.size());
+	int sizeInRT = sizeGarage + sizeInRace;
+
+	cout << "O número de carros na Racetrack é " << sizeInRT;
+
+	return sizeInRT;
+}
+
+
+bool Racetrack::getisCarMax()
+{
+	if (nCars == maxCars)
+	{
+		return true;
+	}
+
+	else
+	{
+		return false;
+	}
+}
+
+int Racetrack::getcarsInTrack()
+{
+	return carsInTrack;
+}
+
+
+int Racetrack::getcarsInGarage()
+{
+	return carsInGarage;
+}
+
+
+int Racetrack::trackToGarage()
+{
+	int times = 0;
+	auto it = std::next(carsInTrack<Car*>(), getnCars()), times++;
+	std::move(carsInTrack<Car*>.begin(), it, std::back_inserter(carsInGarage<Car*>));
+	carsInTrack<Car*>.erase(carsInTrack.begin(), it);
+	cout << "Foram " << times << "carros para a garagem";
+	return times;
+}
+
+
+int Racetrack::garageToTrack()
+{
+	int times = 0;
+	auto it = std::next(carsInGarage(), getnCars()), times++;
+	std::move(carsInGarage.begin(), it, std::back_inserter(carsInTrack<Car*>));
+	carsInGarage.erase(carsInGarage.begin(), it);
+	cout << "Foram " << times << "carros para a track";
+	return times;
+}
+
+
+int Racetrack::moveRaceTrack() {
+
+	std::vector<int> Racetrack::getinRace()
+	{
+		return std::vector<string> inRace;
+		/*if(returned == car called){
+			cout << "Não pode mover o carro porque ele está neste momento a correr";
+		}
+
+		else{
+			//Mover o carro
+		}
+		*/
+	}
+
+}
