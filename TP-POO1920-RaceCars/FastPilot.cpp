f6e2321df290243889b649565c47adc688b61cfa
Diogo <dlsequeira75@gmail.com>
2020-01-05 23:55:23 +0000

    Changed: FastPilot
    Implemented movement and reset

--- TP-POO1920-RaceCars/FastPilot.cpp
+++ TP-POO1920-RaceCars/FastPilot.cpp
@@ -1,5 +1,8 @@
 #include "FastPilot.h"
 #include "Car.h"
+#include "Racetrack.h"
+#include <cstdlib>
+#include <ctime>
 
 FastPilot::FastPilot(const std::string& newName) : Pilot(newName)
 {
@@ -7,6 +10,28 @@ FastPilot::FastPilot(const std::string& newName) : Pilot(newName)
 
 FastPilot::FastPilot(const Pilot& src) : Pilot(src)
 {
+}
+
+bool FastPilot::generatePanicAttack()
+{
+	std::srand(static_cast<unsigned>(time(0)));
+
+	int num = (rand() % 10) + 1;
+
+	std::string tmpString = "O piloto rapido ";
+	tmpString += getName();
+	tmpString += " gerou o numero ";
+	tmpString += std::to_string(num);
+	tmpString += "para gerar um ataque de panico. ";
+	if(num == 7)
+	{
+		tmpString += "O piloto teve um ataque de panico.";
+		setLog(tmpString);
+		return true;
+	}
+	tmpString += "O piloto nao teve nenhum ataque de panico.";
+	setLog(tmpString);
+	return false;
 }
 
 std::string FastPilot::getAsString() const
@@ -41,4 +66,61 @@ std::string FastPilot::getAsString() const
 std::string FastPilot::getTypeAsString() const
 {
 	return "rapido";
+}
+
+bool FastPilot::movement(Racetrack* r)
+{
+	int tmpTime = r->getTime();
+	Car* c = getCurrentCar();
+
+	double currentEnergy = c->getEnergy();
+	double maxEnergy = c->getMaxEnergy();
+	
+	
+	double energyRatio = currentEnergy / maxEnergy;
+
+	if (!getStopOrder())
+	{
+		if (energyRatio > .5)
+		{
+			if (!c->pressAccelerate(2))
+			{
+				c->pressBrake(-2);
+			}
+		}
+		else
+		{
+			if (c->getAccelerationPedal() > 0)
+				c->pressBrake(-2);
+			else
+			{
+				if (tmpTime % 3 == 0)
+				{
+					c->pressAccelerate(1);
+				}
+			}
+		}
+	}
+	else
+	{
+		c->resetPedals();
+		if (c->getSpeed() > 0)
+			c->pressBrake();
+	}
+
+	if (tmpTime % 10 == 0)
+	{
+		if (generatePanicAttack())
+		{
+			c->turnOnEmergencySignal();
+		}
+	}
+	
+	return true;
+}
+
+void FastPilot::reset()
+{
+	setStop(false);
+	setLog("");
 }
